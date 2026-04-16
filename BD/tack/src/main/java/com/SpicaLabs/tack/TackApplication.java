e85ddc44555bf246fc90fee3c5c7eb414c822e6d
Sidd <tahasidd33@gmail.com>
2026-04-17 02:51:53 +0300

    Init

---
+++ BD/tack/src/main/java/com/SpicaLabs/tack/TackApplication.java
@@ -0,0 +1,13 @@
+package com.SpicaLabs.tack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TackApplication.class, args);
+	}
+
+}
---
+++ BD/tack/src/test/java/com/SpicaLabs/tack/TackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SpicaLabs.tack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
