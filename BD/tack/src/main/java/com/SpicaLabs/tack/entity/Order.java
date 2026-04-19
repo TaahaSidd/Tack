c7e2f1c4c528aca2518ce226732782de79000218
Sidd <tahasidd33@gmail.com>
2026-04-20 01:34:26 +0300

    Create Order.java

---
+++ BD/tack/src/main/java/com/SpicaLabs/tack/entity/Order.java
@@ -0,0 +1,31 @@
+package com.SpicaLabs.tack.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Data
+@Table
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String customerName;
+    private LocalDate orderDate;
+
+    @OneToMany(mappedBy = "order")
+    private List<OrderItem> orderItems = new ArrayList<>();
+
+}
